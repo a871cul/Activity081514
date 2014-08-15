76619a634a19653e1b3d1b89a596c36854443673
Cherry Jane A. lora <qwerty_q000@yahoo.com>
2014-08-15 10:34:02 +0800

    im awesome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+New081514::Application.config.secret_key_base = 'ca6cb969e11d546d1aa0d6ded81a957eae4f26c3a71833e343cb6aad24b22eb0d9c4278a4b78110aa10768ddb677f77d1f3d500806375c34cefde889921ac58d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+New081514::Application.config.session_store :cookie_store, key: '_new081514_session'
